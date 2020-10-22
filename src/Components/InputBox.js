897d9892c23c7ea3f4d0d523ee9f64f98b8e9775
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-10-23 00:37:52 +0100

    basic components created

---
+++ src/Components/InputBox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InputBox extends Component {
+  render() {
+    return (
+      <div>
+      <input type="text" placeholder="Shorten all your links with ease"/>
+    </div>
+    )
+  }
+}
+
+export default InputBox
