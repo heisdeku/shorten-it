b6573698e58e4a86bd77e16843a37f585d09f3ef
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-10-23 01:01:45 +0100

    staic styles +

---
+++ src/Components/Results/Result.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Result.css'
+function Result() {
+  return (
+    <div class="result__inner">
+      <div className="result__inner-input">
+        <p className="input">https://twitter.com/adeheka</p>
+      </div>
+      <div className="result__inner-output">
+        <p className="output">https://shrt.web/35fd23</p>
+      </div>
+    </div>
+  )
+}
+
+export default Result;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,22 @@ import { Link } from 'react-router-dom';
 
 import Button from '../Components/Button'
 import InputBox from '../Components/InputBox';
+import Result from '../Components/Results/Result';
 
 const Home = () => 
   <div className="container">
-      <h2>Shorten It</h2>
+    <div className="container__landing">
+    <h2>Shorten <span class="bg">IT</span></h2>
       <p>Shorten all your links <span className="underline">with ease</span></p>
       <InputBox />
       <Button />
       <p>
         <Link to="/mylinks">Check Out All Your Processed links</Link>
       </p>
-      
+    </div>
+      <div className="container__result">
+        <Result />
+      </div>  
   </div>
 
 
