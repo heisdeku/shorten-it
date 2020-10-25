b029caa676d2032db5eab5454f78622bb9851b73
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-10-25 23:03:55 +0100

    +feat: initialize state and logic in all components

--- src/Components/InputBox.js
+++ src/Components/InputBox.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class InputBox extends Component {
+  
   render() {
+    const { value } = this.props;
     return (
       <div className="input-box">
-       <input type="text" placeholder="Shorten all your links with ease"/>
+        <input 
+          type="text" 
+          placeholder="Shorten all your links with ease" 
+          value={value}
+        /> 
       </div>
     )
   }
 }
 
-export default InputBox
+export default InputBox;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,39 @@ import Button from '../Components/Button'
 import InputBox from '../Components/InputBox';
 import Result from '../Components/Results/Result';
 
-const Home = () => 
-  <div className="container">
-    <div className="container__landing">
-    <h2>Shorten <span class="bg">IT</span></h2>
-      <p>Shorten all your links <span className="underline">with ease</span></p>
-      <InputBox />
-      <Button />      
-    </div>
-      <div className="container__result">
-        <h3>Work So Far</h3>
-        <Result
-          input='https://twitter.com/adeheka'
-          output='https://shrt.web/35fd23'
-        />
-      </div>  
-      <p>
+class Home extends React.Component {
+  state = {
+    url: '',
+    processing: false,
+    shortenedUrl: ''
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="container__landing">
+          <h2>Shorten <span class="bg">IT</span></h2>
+          <p>Shorten all your links <span className="underline">with ease</span></p>
+          <InputBox value={this.state.url} />
+          <Button />      
+        </div>
+        <div className="container__result">
+          <h3>Work So Far</h3>
+          { if (shortenedUrl) {
+              <Result
+              input='https://twitter.com/adeheka'
+              output='https://shrt.web/35fd23'
+            />
+          } else {
+            return null;
+          } }          
+        </div>  
+        <p>
         <Link to="/mylinks">Check Out All Your Processed links</Link>
       </p>
   </div>
-
+    )
+}
+}
 
 export default Home
 
