b496c161b61ba0b9d0c56edd616c9acae28ab4c7
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-11-02 03:36:01 +0100

    little thinking and my head hurts

--- src/pages/MyLinks.js
+++ src/pages/MyLinks.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
+
 function MyLinks() {
   let answer = localStorage.getItem("url") || []
   let result = localStorage.getItem('result') || [];
   return (
-    <div className="container">
+    <div className="container-links">
+      <div className="header">
       <h1>Welcome to Your Links Page</h1>
       <p>These are the links you have shortened so far. Kindly thank my creator for these.</p>
-      <table>
+      </div>      
+      <table className="table-container">
         <th>Link</th>
-        <th>shortened Link</th>
+        <th>Shortened Link</th>
         <tr>
-          <td>https://localhost.com</td>
-          <td>https://chickenalhost.com</td>
+          <td><a href="https://localhost.com/3000">https://localhost.com</a></td>
+          <td><a href="https://chickenost.com/3000">https://localhost.com</a></td>
         </tr>
       </table>        
         <p>{result}</p>
